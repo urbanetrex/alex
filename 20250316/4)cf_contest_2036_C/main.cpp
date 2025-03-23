22a6d11f2690a90bafa1d57f66cd444f9132fbd3
urbanetrex <urbanetrex@gmail.com>
2025-03-23 14:13:54 +1100

    starting to test

--- 20250316/4)cf_contest_2036_C/main.cpp
+++ 20250316/4)cf_contest_2036_C/main.cpp
@@ -11,9 +11,14 @@ void solve(){
         i--;
         s[i] = v;
         int n = s.size();
+        bool valid = false;
         for(int i = 0; i<n-3; i++){
-            
+            if(s[i]==1 && s[i+1]==1 && s[i+2]==0 && s[i+3]==0){
+                valid = true;
+                break;
+            }
         }
+        cout<<(valid?"YES":"NO")<<endl;
     }
 }
 int main(){

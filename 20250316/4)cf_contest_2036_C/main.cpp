361e6447362c0aae58476809d7d18b607f588e67
urbanetrex <urbanetrex@gmail.com>
2025-03-23 15:25:58 +1100

    tleed code

--- 20250316/4)cf_contest_2036_C/main.cpp
+++ 20250316/4)cf_contest_2036_C/main.cpp
@@ -14,9 +14,7 @@ void solve(){
         int n = s.size();
         bool valid = false;
         for(int i = 0; i<n-3; i++){
-            cout<<s[i]<<s[i+1]<<s[i+2]<<s[i+3]<<endl;
-            cout<<(s[i]==1 && s[i+1]==1 && s[i+2]==0 && s[i+3]==0)<<endl;
-            if(s[i]==1 && s[i+1]==1 && s[i+2]==0 && s[i+3]==0){
+            if(s[i]=='1' && s[i+1]=='1' && s[i+2]=='0' && s[i+3]=='0'){
                 valid = true;
                 break;
             }

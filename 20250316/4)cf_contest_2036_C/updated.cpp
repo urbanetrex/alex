915a85cd5fbd0f54c5c6e7d1f96e3d2e2475710c
urbanetrex <urbanetrex@gmail.com>
2025-03-23 17:29:22 +1100

    Copying, modifying

--- 20250316/4)cf_contest_2036_C/updated.cpp
+++ 20250316/4)cf_contest_2036_C/updated.cpp
@@ -1,22 +1,29 @@
 #include <iostream>
 #include <vector>
 using namespace std;
+bool is_1100(int i, string &s){
+    if(i+3>=s.size()) return false;
+    return s[i]=='1' && s[i+1]=='1' && s[i+2]=='0' && s[i+3]=='0';
+}
 void solve(){
     string s;
     cin>>s;
-    int q;
+    int q, cnt=0;
+    for(int i = 0; i<s.size()-3; i++){
+        if(is_1100(i, s)){
+            cnt++;
+        }
+    }
     cin>>q;
     while(q--){
         int _i;
         char v;
         cin>>_i>>v;
         _i--;
+        if(is_1100(_i, s))cnt--;
         s[_i] = v;
-        bool valid = false;
-        if(s.find("1100")!=string::npos){
-            valid = true;
-        }
-        cout<<(valid?"YES":"NO")<<endl;
+        if(is_1100(_i, s))cnt++;
+        cout<<(cnt?"YES":"NO")<<endl;
     }
 }
 int main(){

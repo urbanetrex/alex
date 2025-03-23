3bbfb7389b49506555daa1ba48fe2f5fc4d4a133
urbanetrex <urbanetrex@gmail.com>
2025-03-23 13:34:53 +1100

    solving

--- 20250316/4)cf_contest_2036_C/main.cpp
+++ 20250316/4)cf_contest_2036_C/main.cpp
@@ -1,5 +1,21 @@
 #include <iostream>
 using namespace std;
+void solve(){
+    string s;
+    cin >> s;
+    int q;
+    cin>>q;
+    while(q--){
+        int i,v;
+        cin>>i>>v;
+        i--;
+        s[i] = v;
+        int n = s.size();
+        for(int i = 0; i<n-3; i++){
+            
+        }
+    }
+}
 int main(){
-    
+
 }

7f0f7924a62c8cd74c861ffa02ed715bff79494b
urbanetrex <urbanetrex@gmail.com>
2025-03-23 16:58:45 +1100

    updating and testing

--- 20250316/4)cf_contest_2036_C/updated.cpp
+++ 20250316/4)cf_contest_2036_C/updated.cpp
@@ -1,5 +1,29 @@
-#include<iostream>
+#include <iostream>
+#include <vector>
 using namespace std;
+void solve(){
+    string s;
+    cin>>s;
+    int q;
+    cin>>q;
+    while(q--){
+        int _i;
+        char v;
+        cin>>_i>>v;
+        _i--;
+        s[_i] = v;
+        bool valid = false;
+        if(s.find("1100")!=string::npos){
+            valid = true;
+        }
+        cout<<(valid?"YES":"NO")<<endl;
+    }
+}
 int main(){
-    
+    int t;
+    cin>>t;
+    while(t--){
+        solve();
+    }
+    return 0;
 }

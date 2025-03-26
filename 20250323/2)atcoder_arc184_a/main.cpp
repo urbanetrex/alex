a25d2f6a15f8cfa6de5536ec8975d5a05ea3e74e
urbanetrex <urbanetrex@gmail.com>
2025-03-27 00:02:06 +1100

    oh block

--- 20250323/2)atcoder_arc184_a/main.cpp
+++ 20250323/2)atcoder_arc184_a/main.cpp
@@ -2,16 +2,23 @@
 using namespace std;
 int N, M, Q;
 vector<bool> coins;
-vector<bool> known;
-void ask(int a, int b){
+vector< int[3] > known;
+int ask(int a, int b){
     cout << "? " << a << " " << b << endl;
     int res;
     cin >> res;
     if(res==-1) exit(0);
-    known.push_back(res);
+    known.push_back({a,b,res});
+    return res;
 }
 int main(){
     cin >> N >> M >> Q;
     coins.resize(N);
+    for(int i=0; i*2<N; i+=2){
+        if(ask(i, i+1)==1){
+            
+        }
+        
+    }
     
 }

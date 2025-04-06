0208c128372a9ef616f6f4e0acbefb007d57309f
urbanetrex <urbanetrex@gmail.com>
2025-04-06 13:25:20 +1000

    .cpp 1M

--- 20250330/2)cf_contest_2005_B2/main.cpp
+++ 20250330/2)cf_contest_2005_B2/main.cpp
@@ -8,6 +8,17 @@ int main(){
     int t;
     cin >> t;
     while (t--) {
-        
+        int n, m, q;
+        cin >> n >> m >> q;
+        int tc[m];
+        for (int i = 0; i < m; i++) {
+            cin >> tc[i];
+        }
+        for (int i = 0; i < q; i++) {
+            int dq;
+            cin >> dq;
+            
+        }
+
     }
 }
